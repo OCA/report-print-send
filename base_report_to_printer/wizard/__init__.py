c336491c0ecfc24e3aacdc8b84b3cc4ed08c2cf7
Yannick Vaucher <yannick.vaucher@camptocamp.com>
2013-09-30 13:03:49 +0200

    [IMP] base_report_to_printer - clean imports

--- base_report_to_printer/wizard/__init__.py
+++ base_report_to_printer/wizard/__init__.py
@@ -20,4 +20,4 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-import update_printers
+from . import update_printers
