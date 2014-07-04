eae8ee6bcf6c6f7f09cdfefd45cb8d46600ae6e8
Yannick Vaucher <yannick.vaucher@camptocamp.com>
2014-07-04 17:16:51 +0200

    set installable to false

--- printer_tray/__openerp__.py
+++ printer_tray/__openerp__.py
@@ -49,7 +49,7 @@ Report to printer - Paper tray selection
      'ir_report_view.xml',
      ],
  'test': [],
- 'installable': True,
+ 'installable': False,
  'auto_install': False,
  'application': True,
  }
