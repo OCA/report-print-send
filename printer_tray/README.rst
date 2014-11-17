b6fea158c3abd0a2cc6625b49aecc0cbb82240ce
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2014-11-17 13:42:10 +0100

    Extract the module's description in a REAME.rst file

---
+++ printer_tray/README.rst
@@ -0,0 +1,15 @@
+Report to printer - Paper tray selection
+========================================
+
+**Author:** Camptocamp SA
+
+*This module extends `Report to printer` module.*
+
+It detects trays on printer installation plus permits to select the
+paper source on which you want to print directly.
+
+You will find this option on default user config, on default report
+config and on specific config per user per report.
+
+This allows you to dedicate a specific paper source for exemple for
+preprinted paper such as payment slip.
--- printer_tray/__openerp__.py
+++ printer_tray/__openerp__.py
@@ -21,23 +21,6 @@
 {'name': 'Report to printer - Paper tray selection',
  'version': '1.0',
  'category': 'Printer',
- 'description': """
-Report to printer - Paper tray selection
-========================================
-
- **Author:** Camptocamp SA
-
- *This module extends `Report to printer` module.*
-
- It detects trays on printer installation plus permits to select
- the paper source on which you want to print directly.
-
- You will find this option on default user config, on default report config
- and on specific config per user per report.
-
- This allows you to dedicate a specific paper source for exemple for prepinted
- paper such as payment slip.
- """,
  'author': 'Camptocamp',
  'maintainer': 'Camptocamp',
  'website': 'http://www.camptocamp.com/',
