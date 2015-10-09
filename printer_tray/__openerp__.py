197fdfd9c32b17a41aae2d52fb243c24e1f3aa56
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2015-10-09 10:02:55 +0200

    [UPD] prefix versions with 8.0

--- printer_tray/__openerp__.py
+++ printer_tray/__openerp__.py
@@ -19,7 +19,7 @@
 #
 ##############################################################################
 {'name': 'Report to printer - Paper tray selection',
- 'version': '1.0',
+ 'version': '8.0.1.0.0',
  'category': 'Printer',
  'author': "Camptocamp,Odoo Community Association (OCA)",
  'maintainer': 'Camptocamp',
