2214ce2df5587169f7d3254a479c721c61262be5
Dave Lasley <dave@laslabs.com>
2016-07-04 14:36:57 -0700

    [MIG] base_report_to_printer: Upgrade to v9

---
+++ base_report_to_printer/models/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+from . import ir_actions_report_xml
+from . import printing_action
+from . import printing_printer
+from . import printing_report_xml_action
+from . import report
+from . import res_users
