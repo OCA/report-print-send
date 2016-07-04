ecc844e046807c389c43d06d52e881afe0f4ae10
Dave Lasley <dave@laslabs.com>
2016-07-04 14:36:57 -0700

    [MIG] base_report_to_printer: Upgrade to v9

--- base_report_to_printer/models/ir_actions_report_xml.py
+++ base_report_to_printer/models/ir_actions_report_xml.py
@@ -1,41 +1,22 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    Copyright (c) 2007 Ferran Pegueroles <ferran@pegueroles.com>
-#    Copyright (c) 2009 Albert Cervera i Areny <albert@nan-tic.com>
-#    Copyright (C) 2011 Agile Business Group sagl (<http://www.agilebg.com>)
-#    Copyright (C) 2011 Domsense srl (<http://www.domsense.com>)
-#    Copyright (C) 2013-2014 Camptocamp (<http://www.camptocamp.com>)
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published
-#    by the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
-import logging
+# Copyright (c) 2007 Ferran Pegueroles <ferran@pegueroles.com>
+# Copyright (c) 2009 Albert Cervera i Areny <albert@nan-tic.com>
+# Copyright (C) 2011 Agile Business Group sagl (<http://www.agilebg.com>)
+# Copyright (C) 2011 Domsense srl (<http://www.domsense.com>)
+# Copyright (C) 2013-2014 Camptocamp (<http://www.camptocamp.com>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from openerp import models, fields, api
 
-_logger = logging.getLogger('base_report_to_printer')
 
-
-class ReportXml(models.Model):
+class IrActionsReportXml(models.Model):
     """
     Reports
     """
 
     _inherit = 'ir.actions.report.xml'
 
-    property_printing_action = fields.Many2one(
+    property_printing_action_id = fields.Many2one(
         comodel_name='printing.action',
         string='Action',
         company_dependent=True,
@@ -91,7 +72,7 @@ class ReportXml(models.Model):
             printer = default_printer
 
             # Retrieve report default values
-            report_action = report.property_printing_action
+            report_action = report.property_printing_action_id
             if report_action and report_action.type != 'user_default':
                 action = report_action.type
             if report.printing_printer_id:
