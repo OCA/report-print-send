1f00b64974931433f6cba67ead40ea495cd2579c
Bernard Delhez <bernarddelhez@gmail.com>
2016-08-19 14:32:11 +0200

    update version to 9

--- base_report_to_printer/__openerp__.py
+++ base_report_to_printer/__openerp__.py
@@ -23,7 +23,7 @@
 ##############################################################################
 {
     'name': "Report to printer",
-    'version': '8.0.0.1.1',
+    'version': '9.0.1.0',
     'category': 'Generic Modules/Base',
     'author': "Agile Business Group & Domsense, Pegueroles SCP, NaN,"
               "Odoo Community Association (OCA)",
