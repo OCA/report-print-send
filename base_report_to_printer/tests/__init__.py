85e5da91f0a8063fedfacf84485cae2708adfa87
Sylvain Garancher <sylvain.garancher@syleam.fr>
2016-11-09 00:02:46 +0100

    Merge syleam printers module into base_report_to_printer (#60)
    * [IMP] Updated unit tests
    
    * [FIX] Fixed renamed attributes
    
    * [FIX] Remove deleted fields
    
    * [IMP] Add printing.server and printing.job models
    
    * [IMP] Allow to cancel all jobs, enable, and disable printers
    
    * [IMP] Split the cups part of print_document into a new print_file method
    
    * [IMP] Updated cron job to run the action_update_jobs method
    
    * [ADD] Add a migration script to create a printing server from configuration

--- base_report_to_printer/tests/__init__.py
+++ base_report_to_printer/tests/__init__.py
@@ -2,7 +2,9 @@
 # Copyright 2016 LasLabs Inc.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
+from . import test_printing_job
 from . import test_printing_printer
+from . import test_printing_server
 from . import test_report
 from . import test_res_users
 from . import test_ir_actions_report_xml
