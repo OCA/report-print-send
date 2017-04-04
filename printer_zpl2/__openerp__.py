ea041a1f4103a6017333beed0a9bd884eaba9ed8
Sylvain Garancher <sylvain.garancher@syleam.fr>
2017-04-04 19:23:07 +0200

    [ADD] Add printer_zpl2 module (#66)
    * [FIX] printer_tray: Allow to call print_option with no report
    
    * [ADD] Add printer_zpl2 module

---
+++ printer_zpl2/__openerp__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2016 SYLEAM (<http://www.syleam.fr>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Printer ZPL II',
+    'version': '9.0.1.0.0',
+    'category': 'Printer',
+    'author': 'SYLEAM, Odoo Community Association (OCA)',
+    'website': 'http://www.syleam.fr/',
+    'license': 'AGPL-3',
+    'external_dependancies': {
+        'python': ['zpl2'],
+    },
+    'depends': [
+        'base_report_to_printer',
+    ],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/printing_label_zpl2.xml',
+        'wizard/print_record_label.xml',
+    ],
+    'installable': True,
+}
