4a274dc2028fb4b1d02a4bd6eef00c6e7dfc05a9
Sylvain Garancher <sylvain.garancher@syleam.fr>
2017-04-05 18:06:16 +0200

    [10.0][MIG] base_report_to_printer (#68)
    * Set api.multi for action called as `object` on view
    
    * Merge syleam printers module into base_report_to_printer (#60)
    
    * [IMP] Updated unit tests
    
    * [FIX] Fixed renamed attributes
    
    * [FIX] Remove deleted fields
    
    * [IMP] Add printing.server and printing.job models
    
    * [IMP] Allow to cancel all jobs, enable, and disable printers
    
    * [IMP] Split the cups part of print_document into a new print_file method
    
    * [IMP] Updated cron job to run the action_update_jobs method
    
    * [ADD] Add a migration script to create a printing server from configuration
    
    * [MIG] Migrate base_report_to_printer to v10.0
    
    Removed deprecated methods on printing.printer (replaced by methods on
            printing.server)
    
    * [IMP] Add wkhtmltopdf in travis configuration file
    
    * [FIX] base_report_to_printer: Fix Update Job Cron
    * Fix API issue with Update Job Cron
    ** Forward Port from 9.0
    
    * [FIX] Fixed the res.users view
    
    The string attribute should not be used as a selector, because it is
    translatable.
    
    * [FIX] Fixed the print_document method of report
    
    The new API migration was made to @api.multi because of the "cr, uid,
    ids" signature, but "ids" was the ids of the records to print here, not
    the report's ids.
    Also, the new API version of "get_pdf" get directly the ids of the
    records to print in the standard module, not a recordset.
    
    * [FIX] UI is now (un)blocked only when using qweb-pdf reports in standard addons

--- base_report_to_printer/tests/test_ir_actions_report_xml.py
+++ base_report_to_printer/tests/test_ir_actions_report_xml.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 # Copyright 2016 LasLabs Inc.
+# Copyright 2016 SYLEAM
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 import mock
 
-from openerp.tests.common import TransactionCase
+from odoo.tests.common import TransactionCase
 
 
 class TestIrActionsReportXml(TransactionCase):
@@ -14,8 +15,34 @@ class TestIrActionsReportXml(TransactionCase):
         self.Model = self.env['ir.actions.report.xml']
         self.vals = {}
 
-    def new_record(self):
-        return self.Model.create(self.vals)
+        self.report = self.env['ir.actions.report.xml'].search([], limit=1)
+        self.server = self.env['printing.server'].create({})
+
+    def new_action(self):
+        return self.env['printing.action'].create({
+            'name': 'Printing Action',
+            'action_type': 'server',
+        })
+
+    def new_printing_action(self):
+        return self.env['printing.report.xml.action'].create({
+            'report_id': self.report.id,
+            'user_id': self.env.ref('base.user_demo').id,
+            'action': 'server',
+        })
+
+    def new_printer(self):
+        return self.env['printing.printer'].create({
+            'name': 'Printer',
+            'server_id': self.server.id,
+            'system_name': 'Sys Name',
+            'default': True,
+            'status': 'unknown',
+            'status_message': 'Msg',
+            'model': 'res.users',
+            'location': 'Location',
+            'uri': 'URI',
+        })
 
     def test_print_action_for_report_name_gets_report(self):
         """ It should get report by name """
@@ -51,3 +78,131 @@ class TestIrActionsReportXml(TransactionCase):
                 expect, res,
                 'Expect %s, Got %s' % (expect, res),
             )
+
+    def test_behaviour_default_values(self):
+        """ It should return the default action and printer """
+        report = self.Model.search([], limit=1)
+        self.env.user.printing_action = False
+        self.env.user.printing_printer_id = False
+        report.property_printing_action_id = False
+        report.printing_printer_id = False
+        self.assertEqual(report.behaviour(), {
+            report.id: {
+                'action': 'client',
+                'printer': self.env['printing.printer'],
+            },
+        })
+
+    def test_behaviour_user_values(self):
+        """ It should return the action and printer from user """
+        report = self.Model.search([], limit=1)
+        self.env.user.printing_action = 'client'
+        self.env.user.printing_printer_id = self.new_printer()
+        self.assertEqual(report.behaviour(), {
+            report.id: {
+                'action': 'client',
+                'printer': self.env.user.printing_printer_id,
+            },
+        })
+
+    def test_behaviour_report_values(self):
+        """ It should return the action and printer from report """
+        report = self.Model.search([], limit=1)
+        self.env.user.printing_action = 'client'
+        report.property_printing_action_id = self.new_action()
+        report.printing_printer_id = self.new_printer()
+        self.assertEqual(report.behaviour(), {
+            report.id: {
+                'action': report.property_printing_action_id.action_type,
+                'printer': report.printing_printer_id,
+            },
+        })
+
+    def test_behaviour_user_action(self):
+        """ It should return the action and printer from user action"""
+        report = self.Model.search([], limit=1)
+        self.env.user.printing_action = 'client'
+        report.property_printing_action_id.action_type = 'user_default'
+        self.assertEqual(report.behaviour(), {
+            report.id: {
+                'action': 'client',
+                'printer': report.printing_printer_id,
+            },
+        })
+
+    def test_behaviour_printing_action_on_wrong_user(self):
+        """ It should return the action and printer ignoring printing action
+        """
+        report = self.Model.search([], limit=1)
+        self.env.user.printing_action = 'client'
+        printing_action = self.new_printing_action()
+        printing_action.user_id = self.env['res.users'].search([
+            ('id', '!=', self.env.user.id),
+        ], limit=1)
+        self.assertEqual(report.behaviour(), {
+            report.id: {
+                'action': 'client',
+                'printer': report.printing_printer_id,
+            },
+        })
+
+    def test_behaviour_printing_action_on_wrong_report(self):
+        """ It should return the action and printer ignoring printing action
+        """
+        report = self.Model.search([], limit=1)
+        self.env.user.printing_action = 'client'
+        printing_action = self.new_printing_action()
+        printing_action.user_id = self.env.user
+        printing_action.report_id = self.env['ir.actions.report.xml'].search([
+            ('id', '!=', report.id),
+        ], limit=1)
+        self.assertEqual(report.behaviour(), {
+            report.id: {
+                'action': 'client',
+                'printer': report.printing_printer_id,
+            },
+        })
+
+    def test_behaviour_printing_action_with_no_printer(self):
+        """ It should return the action from printing action and printer from other
+        """
+        report = self.Model.search([], limit=1)
+        self.env.user.printing_action = 'client'
+        printing_action = self.new_printing_action()
+        printing_action.user_id = self.env.user
+        printing_action.report_id = report
+        self.assertEqual(report.behaviour(), {
+            report.id: {
+                'action': printing_action.action,
+                'printer': report.printing_printer_id,
+            },
+        })
+
+    def test_behaviour_printing_action_with_printer(self):
+        """ It should return the action and printer from printing action """
+        report = self.Model.search([], limit=1)
+        self.env.user.printing_action = 'client'
+        printing_action = self.new_printing_action()
+        printing_action.user_id = self.env.user
+        printing_action.printer_id = self.new_printer()
+        self.assertEqual(report.behaviour(), {
+            report.id: {
+                'action': printing_action.action,
+                'printer': printing_action.printer_id,
+            },
+        })
+
+    def test_behaviour_printing_action_user_defaults(self):
+        """ It should return the action and printer from user with printing action
+        """
+        report = self.Model.search([], limit=1)
+        self.env.user.printing_action = 'client'
+        printing_action = self.new_printing_action()
+        printing_action.user_id = self.env.user
+        printing_action.action = 'user_default'
+        self.assertEqual(report.behaviour(), {
+            report.id: {
+                'action': 'client',
+                'printer': report.printing_printer_id,
+            },
+        })
--- base_report_to_printer/tests/test_printing_printer.py
+++ base_report_to_printer/tests/test_printing_printer.py
@@ -2,17 +2,15 @@
 # Copyright 2016 LasLabs Inc.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
+import tempfile
 import mock
 
-from openerp.tests.common import TransactionCase
-
-from openerp.addons.base_report_to_printer.models.printing_printer import (
-    CUPS_HOST,
-    CUPS_PORT,
-)
+from odoo.exceptions import UserError
+from odoo.tests.common import TransactionCase
 
 
-model = 'openerp.addons.base_report_to_printer.models.printing_printer'
+model = 'odoo.addons.base_report_to_printer.models.printing_printer'
+server_model = 'odoo.addons.base_report_to_printer.models.printing_server'
 
 
 class TestPrintingPrinter(TransactionCase):
@@ -20,8 +18,11 @@ class TestPrintingPrinter(TransactionCase):
     def setUp(self):
         super(TestPrintingPrinter, self).setUp()
         self.Model = self.env['printing.printer']
+        self.ServerModel = self.env['printing.server']
+        self.server = self.env['printing.server'].create({})
         self.printer_vals = {
             'name': 'Printer',
+            'server_id': self.server.id,
             'system_name': 'Sys Name',
             'default': True,
             'status': 'unknown',
@@ -34,74 +35,110 @@ class TestPrintingPrinter(TransactionCase):
     def new_record(self):
         return self.Model.create(self.printer_vals)
 
-    @mock.patch('%s.cups' % model)
-    def test_update_printers_status_error(self, cups):
-        """ It should catch any exception from CUPS and update status """
+    def test_printing_options(self):
+        """ It should generate the right options dictionnary """
+        self.assertEquals(self.Model.print_options('report', 'raw'), {
+            'raw': 'True',
+        })
+        self.assertEquals(self.Model.print_options('report', 'pdf', 2), {
+            'copies': '2',
+        })
+        self.assertEquals(self.Model.print_options('report', 'raw', 2), {
+            'raw': 'True',
+            'copies': '2',
+        })
+
+    @mock.patch('%s.cups' % server_model)
+    def test_print_report(self, cups):
+        """ It should print a report through CUPS """
+        fd, file_name = tempfile.mkstemp()
+        with mock.patch('%s.mkstemp' % model) as mkstemp:
+            mkstemp.return_value = fd, file_name
+            printer = self.new_record()
+            printer.print_document('report_name', 'content to print', 'pdf')
+            cups.Connection().printFile.assert_called_once_with(
+                printer.system_name,
+                file_name,
+                file_name,
+                options={})
+
+    @mock.patch('%s.cups' % server_model)
+    def test_print_report_error(self, cups):
+        """ It should print a report through CUPS """
         cups.Connection.side_effect = Exception
-        rec_id = self.new_record()
-        self.Model.update_printers_status()
-        self.assertEqual(
-            'server-error', rec_id.status,
+        fd, file_name = tempfile.mkstemp()
+        with mock.patch('%s.mkstemp' % model) as mkstemp:
+            mkstemp.return_value = fd, file_name
+            printer = self.new_record()
+            with self.assertRaises(UserError):
+                printer.print_document(
+                    'report_name', 'content to print', 'pdf')
+
+    @mock.patch('%s.cups' % server_model)
+    def test_print_file(self, cups):
+        """ It should print a file through CUPS """
+        file_name = 'file_name'
+        printer = self.new_record()
+        printer.print_file(file_name, 'pdf')
+        cups.Connection().printFile.assert_called_once_with(
+            printer.system_name,
+            file_name,
+            file_name,
+            options={})
+
+    @mock.patch('%s.cups' % server_model)
+    def test_print_file_error(self, cups):
+        """ It should print a file through CUPS """
+        cups.Connection.side_effect = Exception
+        file_name = 'file_name'
+        printer = self.new_record()
+        with self.assertRaises(UserError):
+            printer.print_file(file_name)
+
+    def test_set_default(self):
+        """ It should set a single record as default """
+        printer = self.new_record()
+        self.assertTrue(printer.default)
+        other_printer = self.new_record()
+        other_printer.set_default()
+        self.assertFalse(printer.default)
+        self.assertTrue(other_printer.default)
+        # Check that calling the method on an empty recordset does nothing
+        self.Model.set_default()
+        self.assertEquals(other_printer, self.Model.get_default())
+
+    @mock.patch('%s.cups' % server_model)
+    def test_cancel_all_jobs(self, cups):
+        """ It should cancel all jobs """
+        printer = self.new_record()
+        printer.action_cancel_all_jobs()
+        cups.Connection().cancelAllJobs.assert_called_once_with(
+            name=printer.system_name,
+            purge_jobs=False,
         )
 
-    @mock.patch('%s.cups' % model)
-    def test_update_printers_status_inits_cups(self, cups):
-        """ It should init CUPS connection """
-        self.new_record()
-        self.Model.update_printers_status()
-        cups.Connection.assert_called_once_with(
-            CUPS_HOST, CUPS_PORT,
+    @mock.patch('%s.cups' % server_model)
+    def test_cancel_and_purge_all_jobs(self, cups):
+        """ It should cancel all jobs """
+        printer = self.new_record()
+        printer.cancel_all_jobs(purge_jobs=True)
+        cups.Connection().cancelAllJobs.assert_called_once_with(
+            name=printer.system_name,
+            purge_jobs=True,
         )
 
-    @mock.patch('%s.cups' % model)
-    def test_update_printers_status_gets_all_printers(self, cups):
-        """ It should get all printers from CUPS server """
-        self.new_record()
-        self.Model.update_printers_status()
-        cups.Connection().getPrinters.assert_called_once_with()
+    @mock.patch('%s.cups' % server_model)
+    def test_enable_printer(self, cups):
+        """ It should enable the printer """
+        printer = self.new_record()
+        printer.enable()
+        cups.Connection().enablePrinter.assert_called_once_with(
+            printer.system_name)
 
-    @mock.patch('%s.cups' % model)
-    def test_update_printers_status_gets_printer(self, cups):
-        """ It should get printer from CUPS by system_name """
-        rec_id = self.new_record()
-        self.Model.update_printers_status()
-        cups.Connection().getPrinters().get.assert_called_once_with(
-            rec_id.system_name,
-        )
-
-    @mock.patch('%s.cups' % model)
-    def test_update_printers_status_search(self, cups):
-        """ It should search all when no domain """
-        with mock.patch.object(self.Model, 'search') as search:
-            self.Model.update_printers_status()
-            search.assert_called_once_with([])
-
-    @mock.patch('%s.cups' % model)
-    def test_update_printers_status_search_domain(self, cups):
-        """ It should use specific domain for search """
-        with mock.patch.object(self.Model, 'search') as search:
-            expect = [('id', '>', 0)]
-            self.Model.update_printers_status(expect)
-            search.assert_called_once_with(expect)
-
-    @mock.patch('%s.cups' % model)
-    def test_update_printers_status_update_printer(self, cups):
-        """ It should update from CUPS when printer identified """
-        with mock.patch.object(self.Model, 'search') as search:
-            printer_mk = mock.MagicMock()
-            search.return_value = [printer_mk]
-            self.Model.update_printers_status()
-            printer_mk.update_from_cups.assert_called_once_with(
-                cups.Connection(),
-                cups.Connection().getPrinters().get(),
-            )
-
-    @mock.patch('%s.cups' % model)
-    def test_update_printers_status_update_unavailable(self, cups):
-        """ It should update status when printer is unavailable """
-        rec_id = self.new_record()
-        cups.Connection().getPrinters().get.return_value = False
-        self.Model.update_printers_status()
-        self.assertEqual(
-            'unavailable', rec_id.status,
-        )
+    @mock.patch('%s.cups' % server_model)
+    def test_disable_printer(self, cups):
+        """ It should disable the printer """
+        printer = self.new_record()
+        printer.disable()
+        cups.Connection().disablePrinter.assert_called_once_with(
+            printer.system_name)
