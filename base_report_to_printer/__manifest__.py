0b3aba3f1af22d8b34a6a87b4d691fa756851779
Sylvain GARANCHER <sylvain.garancher@syleam.fr>
2017-06-23 14:37:45 +0200

    [FIX] base_report_to_printer: A button method should be @api.multi

--- base_report_to_printer/__manifest__.py
+++ base_report_to_printer/__manifest__.py
@@ -8,7 +8,7 @@
 
 {
     'name': "Report to printer",
-    'version': '10.0.1.0.0',
+    'version': '10.0.1.0.1',
     'category': 'Generic Modules/Base',
     'author': "Agile Business Group & Domsense, Pegueroles SCP, NaN,"
               " LasLabs, Odoo Community Association (OCA)",
