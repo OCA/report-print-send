d6500e4bfe8daddf7f5ffbf7640cb8a889e0b0c9
Graeme Gellatly <gdgellatly@gmail.com>
2017-10-06 01:16:33 +1300

    [WIP] Commit before rebase on latest #109 for user and view updates
    Migration of printer_tray to v11 and integration with base_report_to_printer

--- base_report_to_printer/models/res_users.py
+++ base_report_to_printer/models/res_users.py
@@ -26,3 +26,14 @@ class ResUsers(models.Model):
     )
     printing_printer_id = fields.Many2one(comodel_name='printing.printer',
                                           string='Default Printer')
+
+    printer_tray_id = fields.Many2one(
+        comodel_name='printing.tray',
+        string='Default Printer Paper Source',
+        domain="[('printer_id', '=', printing_printer_id)]",
+    )
+
+    @api.onchange('printing_printer_id')
+    def onchange_printing_printer_id(self):
+        """ Reset the tray when the printer is changed """
+        self.printer_tray_id = False
--- printer_tray/models/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-# Copyright (C) 2013-2014 Camptocamp (<http://www.camptocamp.com>)
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-
-from . import ir_actions_report
-from . import printing_tray
-from . import printing_printer
-from . import res_users
-from . import printing_report_xml_action
