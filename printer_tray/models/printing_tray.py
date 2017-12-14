c83ff1f0c151916f22dabe401b17188c1c5aa012
tiku <kuczera@geiwiz.de>
2017-12-14 16:55:21 +0100

    working with Odoo 11

---
+++ printer_tray/models/printing_tray.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2013-2014 Camptocamp (<http://www.camptocamp.com>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+
+
+class PrinterTray(models.Model):
+    _name = 'printing.tray'
+    _description = 'Printer Tray'
+
+    name = fields.Char(required=True)
+    system_name = fields.Char(required=True, readonly=True)
+    printer_id = fields.Many2one(
+        comodel_name='printing.printer',
+        string='Printer',
+        required=True,
+        readonly=True,
+        ondelete='cascade',
+    )
---
+++ printer_tray/tests/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 LasLabs Inc.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from . import test_ir_actions_report_xml
+from . import test_printing_printer
+from . import test_printing_report_xml_action
+from . import test_printing_tray
+from . import test_res_users
