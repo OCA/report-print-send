1f78dbce2c4150972a2e1be139602ace62ebee3f
Jairo Llopis <yajo.sk8@gmail.com>
2018-01-11 12:45:15 +0000

    fixup! [FIX] base_report_to_printer: Stateless tests

--- base_report_to_printer/tests/test_report.py
+++ base_report_to_printer/tests/test_report.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 # Copyright 2016 LasLabs Inc.
+# Copyright 2017 Tecnativa - Jairo Llopis
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 import mock
