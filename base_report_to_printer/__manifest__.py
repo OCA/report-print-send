1231c42fd017701cee54c5302d574f44d5bac809
Yannick Vaucher <yannick.vaucher@camptocamp.com>
2018-02-02 17:56:39 +0100

    Bump base_report_to_printer version to 11.0.2.1.0

--- base_report_to_printer/__manifest__.py
+++ base_report_to_printer/__manifest__.py
@@ -8,7 +8,7 @@
 
 {
     'name': "Report to printer",
-    'version': '11.0.2.0.0',
+    'version': '11.0.2.1.0',
     'category': 'Generic Modules/Base',
     'author': "Agile Business Group & Domsense, Pegueroles SCP, NaN,"
               " LasLabs, Camptocamp, Odoo Community Association (OCA)",
