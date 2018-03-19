0c50f6cd55d1780d01996ced6b219ae9535abced
Florent de Labarre <florent.mirieu@gmail.com>
2018-03-19 19:44:32 +0100

    [FIX] extra is not pass

--- printer_zpl2/__manifest__.py
+++ printer_zpl2/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     'name': 'Printer ZPL II',
-    'version': '10.0.1.1.1',
+    'version': '10.0.1.1.2',
     'category': 'Printer',
     'author': 'SYLEAM, Odoo Community Association (OCA)',
     'website': 'http://www.syleam.fr/',
