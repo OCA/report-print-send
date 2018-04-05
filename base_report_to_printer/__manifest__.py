7431198043f850e699e2d81295d9c4b2000de003
Rod Schouteden <rod@schout-it.be>
2018-04-05 16:45:42 +0200

    [9.0][10.0][11.0] [FIX] user access to printer objects (#138)
    This was default behavior in v8 where there was no specific group on the read access of these objects.
    The group was created in v9 so all versions upwards are affected

--- base_report_to_printer/__manifest__.py
+++ base_report_to_printer/__manifest__.py
@@ -8,7 +8,7 @@
 
 {
     'name': "Report to printer",
-    'version': '10.0.1.0.2',
+    'version': '10.0.1.0.3',
     'category': 'Generic Modules/Base',
     'author': "Agile Business Group & Domsense, Pegueroles SCP, NaN,"
               " LasLabs, Odoo Community Association (OCA)",
