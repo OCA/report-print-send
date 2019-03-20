60293ad51d1b9553805be9167544db1de6ef7619
Florent de Labarre <florent.mirieu@gmail.com>
2019-03-20 14:24:37 +0100

    [MIG] printer_zpl2: Migration to 12.0

--- printer_zpl2/__manifest__.py
+++ printer_zpl2/__manifest__.py
@@ -1,11 +1,12 @@
-# Copyright (C) 2016 SYLEAM (<http://www.syleam.fr>)
+# Copyright (C) 2016 SUBTENO-IT (<https://subteno-it.fr>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'Printer ZPL II',
-    'version': '11.0.1.0.0',
+    'version': '12.0.1.0.0',
     'category': 'Printer',
-    'author': 'SYLEAM, Apertoso NV, Odoo Community Association (OCA)',
+    'author': 'SUBTENO-IT, FLorent de Labarre, '
+              'Apertoso NV, Odoo Community Association (OCA)',
     'website': 'http://www.syleam.fr/',
     'license': 'AGPL-3',
     'external_dependencies': {
