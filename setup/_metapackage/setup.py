d5bbfed58b17d99bfaef8738dc123929bd5a13f0
Rod Schouteden <rod.schouteden@dynapps.be>
2019-09-30 15:35:28 +0200

    [MIG] printer_zpl2: Migration to 13.0

--- setup/_metapackage/setup.py
+++ setup/_metapackage/setup.py
@@ -1,18 +1,12 @@
 import setuptools
 
-with open('VERSION.txt', 'r') as f:
+with open("VERSION.txt", "r") as f:
     version = f.read().strip()
 
 setuptools.setup(
     name="odoo13-addons-oca-report-print-send",
     description="Meta package for oca-report-print-send Odoo addons",
     version=version,
-    install_requires=[
-        'odoo13-addon-base_report_to_printer',
-        'odoo13-addon-stock_picking_auto_print',
-    ],
-    classifiers=[
-        'Programming Language :: Python',
-        'Framework :: Odoo',
-    ]
+    install_requires=["odoo13-addon-base_report_to_printer"],
+    classifiers=["Programming Language :: Python", "Framework :: Odoo"],
 )
