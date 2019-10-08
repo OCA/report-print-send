78e9994e04691cdcd3e818a34eeed5713bbe3c33
Raf Ven <raf.ven@dynapps.be>
2019-10-08 11:51:13 +0200

    [12.0-issue-183-base] [12.0] base_report_to_printer: Do no print e-mail attachment

---
+++ base_report_to_printer_mail/__manifest__.py
@@ -0,0 +1,18 @@
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': "Report to printer - Mail extension",
+    'version': '12.0.1.0.0',
+    'category': 'Generic Modules/Base',
+    'author': "DynApps NV, Odoo Community Association (OCA)",
+    'website': 'https://github.com/OCA/report-print-send',
+    'license': 'AGPL-3',
+    "depends": [
+        'mail',
+        'base_report_to_printer',
+    ],
+    'data': [
+    ],
+    'installable': True,
+    'auto_install': True,
+}
