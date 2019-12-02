ce608c61ded2015b5c71c526dd5459f994a23ac9
Bhavesh Odedra <bodedra@opensourceintegrators.com>
2019-12-02 20:04:35 +0530

    Update stock_picking_auto_print/__manifest__.py
    Co-Authored-By: Maxime Chambreuil <mchambreuil@opensourceintegrators.com>

--- stock_picking_auto_print/__manifest__.py
+++ stock_picking_auto_print/__manifest__.py
@@ -11,7 +11,6 @@
     'category': 'Generic Modules/Base',
     'website': 'http://www.opensourceintegrators.com',
     'depends': [
-        'stock',
         'sale_stock',
         'base_report_to_printer',
     ],
