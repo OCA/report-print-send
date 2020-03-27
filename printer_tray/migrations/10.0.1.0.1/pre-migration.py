c41051de1a42b060405eb2a366c9697505a0a090
Stephane <eicher31@hotmail.com>
2020-03-27 13:41:52 +0100

    Fix migration

--- printer_tray/migrations/10.0.1.0.1/pre-migration.py
+++ printer_tray/migrations/10.0.1.0.1/pre-migration.py
@@ -6,7 +6,7 @@ from openupgradelib import openupgrade
 def migrate(cr, version):
     if not version:
         return
-
+    openupgrade.rename_tables(cr, [('printing_tray', 'printing_tray_input')])
     openupgrade.rename_models(cr, [
         ('printing.tray', 'printing.tray.input'),
     ])
