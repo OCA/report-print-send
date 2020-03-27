752f89cb104913ccc68d7955bad36319cdad538e
Emanuel Cino <ecino@compassion.ch>
2020-03-27 16:37:16 +0100

    [10.0][ADD] Report action defined per language
    This enables to change the print configuration depending on the language. It is useful for printing documents differently depending on the language of the partner for instance.

--- base_report_to_printer/models/printing_report_xml_action.py
+++ base_report_to_printer/models/printing_report_xml_action.py
@@ -6,7 +6,8 @@
 # Copyright (C) 2013-2014 Camptocamp (<http://www.camptocamp.com>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import models, fields, api
+from odoo import models, fields, api, _
+from odoo.exceptions import UserError
 
 
 class PrintingReportXmlAction(models.Model):
@@ -19,8 +20,8 @@ class PrintingReportXmlAction(models.Model):
                                 ondelete='cascade')
     user_id = fields.Many2one(comodel_name='res.users',
                               string='User',
-                              required=True,
                               ondelete='cascade')
+    language_id = fields.Many2one('res.lang', 'Language')
     action = fields.Selection(
         selection=lambda s: s.env['printing.action']._available_action_types(),
         required=True,
@@ -28,11 +29,50 @@ class PrintingReportXmlAction(models.Model):
     printer_id = fields.Many2one(comodel_name='printing.printer',
                                  string='Printer')
 
+    _sql_constraints = [(
+        'unique_configuration', 'unique(report_id,user_id,language_id)',
+        'There is already an action defined for this user/language.'
+    )]
+
+    @api.constrains('user_id', 'language_id')
+    def check_action(self):
+        for action in self:
+            if not action.user_id and not action.language_id:
+                raise UserError(_(
+                    "You should set a user or a language for which this action "
+                    "applies to."))
+
     @api.multi
     def behaviour(self):
-        if not self:
+        action = self
+        if len(action) > 1:
+            action = self.select_action()
+        if not action:
             return {}
         return {
-            'action': self.action,
-            'printer': self.printer_id,
+            'action': action.action,
+            'printer': action.printer_id,
         }
+
+    @api.multi
+    def select_action(self):
+        """
+        Returns the action that makes the most sense for the print depending
+        on the context (user and language).
+        :return: One record of printing.report.xml.action
+        """
+        action = self
+        if len(self) > 1:
+            # Find the most relevant action : user first, language second
+            user_action = self.filtered(lambda a: a.user_id == self.env.user)
+            if user_action:
+                if len(user_action) == 1:
+                    action = user_action
+                else:
+                    user_language_action = user_action.filtered(
+                        lambda a: a.language_id.code == self.env.lang)
+                    if user_language_action:
+                        action = user_language_action
+            else:
+                action = self.filtered(lambda a: a.language_id.code == self.env.lang)
+        return action
