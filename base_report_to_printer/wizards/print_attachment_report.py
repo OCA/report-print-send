d1822016a8adb277da34dc57d4c87ceec16c4ad7
sebalix <seb@usr-src.org>
2020-08-06 12:30:44 +0200

    [MIG] base_report_to_printer: Print attachments wizard

--- base_report_to_printer/wizards/print_attachment_report.py
+++ base_report_to_printer/wizards/print_attachment_report.py
@@ -1,25 +1,26 @@
-# -*- coding: utf-8 -*-
 # Copyright 2020 Camptocamp SA
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl)
 import base64
 
-from openerp import _, models, api, fields
+from odoo import _, fields, models
 
 
 class PrintAttachment(models.TransientModel):
-    _name = 'wizard.print.attachment'
-    _description = 'Print Attachment'
+    _name = "wizard.print.attachment"
+    _description = "Print Attachment"
 
     printer_id = fields.Many2one(
-        comodel_name='printing.printer', string='Printer', required=True,
-        help='Printer used to print the attachments.'
+        comodel_name="printing.printer",
+        string="Printer",
+        required=True,
+        help="Printer used to print the attachments.",
     )
     attachment_line_ids = fields.One2many(
-        'wizard.print.attachment.line', 'wizard_id',
-        string='Attachments to print',
+        "wizard.print.attachment.line",
+        "wizard_id",
+        string="Attachments to print",
     )
 
-    @api.multi
     def print_attachments(self):
         """ Prints a label per selected record """
         self.ensure_one()
@@ -32,18 +33,15 @@ class PrintAttachment(models.TransientModel):
             content = base64.b64decode(data)
             content_format = att_line.get_format()
             self.printer_id.print_document(
-                None,
-                content=content,
-                format=content_format,
-                copies=att_line.copies
+                None, content=content, format=content_format, copies=att_line.copies
             )
         if errors:
             return {
-                'warning': _(
-                    'Following attachments could not be printed:\n\n%s'
-                    % '\n'.join(
+                "warning": _(
+                    "Following attachments could not be printed:\n\n%s"
+                    % "\n".join(
                         [
-                            _('%s (%s copies)') % (err.record_name, err.copies)
+                            _("%s (%s copies)") % (err.record_name, err.copies)
                             for err in errors
                         ]
                     )
@@ -52,23 +50,25 @@ class PrintAttachment(models.TransientModel):
 
 
 class PrintAttachmentLine(models.TransientModel):
-    _name = 'wizard.print.attachment.line'
-    _description = 'Print Attachment line'
+    _name = "wizard.print.attachment.line"
+    _description = "Print Attachment line"
 
     wizard_id = fields.Many2one("wizard.print.attachment")
     attachment_id = fields.Many2one(
-        'ir.attachment',
+        "ir.attachment",
         required=True,
-        domain="['|', ('mimetype', '=', 'application/pdf'), ('mimetype', '=', 'application/octet-stream')]"
+        domain=(
+            "['|', ('mimetype', '=', 'application/pdf'), "
+            "('mimetype', '=', 'application/octet-stream')]"
+        ),
     )
     record_name = fields.Char(related="attachment_id.res_name", readonly=True)
     copies = fields.Integer(default=1)
 
-    @api.multi
     def get_format(self):
         self.ensure_one()
         mimetype = self.attachment_id.mimetype
         if mimetype == "application/pdf":
             return "pdf"
         else:
-            return 'raw'
+            return "raw"
