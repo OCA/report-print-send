b265e58784d2370efa4acf039cac3f927d09a503
Jaime Arroyo <jaime.arroyo@creublanca.es>
2020-09-29 12:45:51 +0200

    [IMP] base_report_to_printer_mail: black, isort, prettier

--- base_report_to_printer_mail/models/mail_template.py
+++ base_report_to_printer_mail/models/mail_template.py
@@ -5,6 +5,6 @@ class MailTemplate(models.Model):
     _inherit = "mail.template"
 
     def generate_email(self, res_ids, fields=None):
-        return super(MailTemplate, self.with_context(
-            must_skip_send_to_printer=True
-        )).generate_email(res_ids, fields=fields)
+        return super(
+            MailTemplate, self.with_context(must_skip_send_to_printer=True)
+        ).generate_email(res_ids, fields=fields)
