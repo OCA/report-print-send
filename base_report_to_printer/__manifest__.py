03b08ec40234b17805d5992f66cac63a777430ca
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2021-02-04 13:54:13 +0100

    [IMP] base_report_to_printer: Avoid writing on printing printers/jobs if no change
    The cron (by default) every minute, updates the printing.printer and
    printing.job records from the cups server. The field values rarely
    change, so we can prevent many updates in database.
    
    Also, writing on `printing_printer.server_id` triggers another update
    on the printing.job records as the model has a stored related field.

--- base_report_to_printer/__manifest__.py
+++ base_report_to_printer/__manifest__.py
@@ -7,7 +7,7 @@
 
 {
     "name": "Report to printer",
-    "version": "14.0.1.0.0",
+    "version": "14.0.1.0.1",
     "category": "Generic Modules/Base",
     "author": "Agile Business Group & Domsense, Pegueroles SCP, NaN,"
     " LasLabs, Camptocamp, Odoo Community Association (OCA),"
