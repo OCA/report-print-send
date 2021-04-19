47f39822e72a8d73b68ba203cb7ab63f39404883
Harald Welte <laforge@osmocom.org>
2021-04-19 20:46:01 +0200

    base_report_to_printer_mail: avoid breaking password recovery
    Let's use the Odoo 7 API to avoid breaking the password recovery
    link.
    
    Closes: SYS#5433

--- base_report_to_printer_mail/models/mail_template.py
+++ base_report_to_printer_mail/models/mail_template.py
@@ -1,10 +1,10 @@
-from openerp import models
+from openerp.osv import osv, fields
 
-
-class MailTemplate(models.Model):
+class MailTemplate(osv.osv):
     _inherit = "email.template"
 
-    def generate_email(self, res_ids, fields=None):
-        return super(MailTemplate, self.with_context(
-            must_skip_send_to_printer=True
-        )).generate_email(res_ids, fields=fields)
+    def generate_email(self, cr, uid, template_id, res_id, context=None):
+        if context is None:
+            context = {}
+        context.update({'must_skip_send_to_printer': True})
+        return super(MailTemplate, self).generate_email(cr, uid, template_id, res_id, context)
