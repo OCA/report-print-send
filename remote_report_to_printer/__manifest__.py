70f83d82e9362bf47e621bb587796bbac0fd2581
Olga Marco <olga.marco@creublanca.es>
2021-06-17 16:57:44 +0200

    [IMP] remote_report_to_printer: black, isort, prettier

--- remote_report_to_printer/__manifest__.py
+++ remote_report_to_printer/__manifest__.py
@@ -2,18 +2,18 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': "Report to printer on remotes",
-    'version': '12.0.2.1.1',
-    'category': 'Generic Modules/Base',
-    'author': "Creu Blanca, Odoo Community Association (OCA)",
-    'website': 'http://www.github.com/OCA/report-print-send',
-    'license': 'AGPL-3',
-    "depends": ['base_remote', 'base_report_to_printer'],
-    'data': [
-        'data/printing_data.xml',
-        'security/ir.model.access.csv',
-        'views/res_remote_views.xml',
-        'views/res_remote_printer_views.xml',
+    "name": "Report to printer on remotes",
+    "version": "13.0.1.0.0",
+    "category": "Generic Modules/Base",
+    "author": "Creu Blanca, Odoo Community Association (OCA)",
+    "website": "http://www.github.com/OCA/report-print-send",
+    "license": "AGPL-3",
+    "depends": ["base_remote", "base_report_to_printer"],
+    "data": [
+        "data/printing_data.xml",
+        "security/ir.model.access.csv",
+        "views/res_remote_views.xml",
+        "views/res_remote_printer_views.xml",
     ],
-    'installable': True,
+    "installable": True,
 }
