aebea965ef783419e118c63c92c9f9a614cc4abf
Olga Marco <olga.marco@creublanca.es>
2021-06-17 16:57:44 +0200

    [IMP] remote_report_to_printer: black, isort, prettier

--- remote_report_to_printer/models/ir_actions_report.py
+++ remote_report_to_printer/models/ir_actions_report.py
@@ -5,18 +5,18 @@ from odoo import api, models
 
 
 class IrActionsReport(models.Model):
-    _inherit = 'ir.actions.report'
+    _inherit = "ir.actions.report"
 
     @api.multi
     def _get_user_default_print_behaviour(self):
         res = super()._get_user_default_print_behaviour()
-        if res.get('action', 'unknown') == 'remote_default':
+        if res.get("action", "unknown") == "remote_default":
             res.update(self.remote.get_printer_behaviour())
         return res
 
     @api.multi
     def _get_report_default_print_behaviour(self):
         res = super()._get_report_default_print_behaviour()
-        if res.get('action', 'unknown') == 'remote_default':
+        if res.get("action", "unknown") == "remote_default":
             res.update(self.remote.get_printer_behaviour())
         return res
