1745f8cb46dee82d9534a9c948216f60b00133de
User <pablo.luaces@gmail.com>
2021-08-11 07:54:52 +0000

    [MIG] base_report_to_printer_mail: Migration to 14.0

--- base_report_to_printer_mail/__manifest__.py
+++ base_report_to_printer_mail/__manifest__.py
@@ -2,7 +2,7 @@
 
 {
     "name": "Report to printer - Mail extension",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "category": "Generic Modules/Base",
     "author": "DynApps NV, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/report-print-send",
