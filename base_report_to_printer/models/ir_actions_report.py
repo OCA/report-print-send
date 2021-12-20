e3d1de973037c14d6a7993100043e285ef5eec8e
Denis Roussel <denis.roussel@acsone.eu>
2021-12-20 10:49:34 +0100

    [14.0][FIX] base_report_to_printer: Don't use safe_eval as module

--- base_report_to_printer/models/ir_actions_report.py
+++ base_report_to_printer/models/ir_actions_report.py
@@ -4,9 +4,10 @@
 # Copyright (C) 2011 Domsense srl (<http://www.domsense.com>)
 # Copyright (C) 2013-2014 Camptocamp (<http://www.camptocamp.com>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+from time import time
 
 from odoo import _, api, exceptions, fields, models
-from odoo.tools.safe_eval import safe_eval, time
+from odoo.tools.safe_eval import safe_eval
 
 REPORT_TYPES = {"qweb-pdf": "pdf", "qweb-text": "text"}
 
