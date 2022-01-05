a758c650e48e708ae44e370efe6288462ba5b876
Olga Marco <olga.marco@creublanca.es>
2022-01-05 09:39:55 +0100

    [MIG] remote_report_to_printer: Migration to 14.0

--- remote_report_to_printer/__manifest__.py
+++ remote_report_to_printer/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Report to printer on remotes",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "category": "Generic Modules/Base",
     "author": "Creu Blanca, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/report-print-send",
