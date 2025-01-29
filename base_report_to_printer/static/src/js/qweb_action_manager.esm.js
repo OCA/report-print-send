5d1c12f6c9768b4fadfc4aee9385516f1b9067ed
Vincent Van Rossem <vincent.vanrossem@camptocamp.com>
2025-01-29 12:39:55 +0100

    [FIX] base_report_to_printer: js warning sort-imports
    base_report_to_printer/static/src/js/qweb_action_manager.esm.js
    warning  Imports should be sorted alphabetically  sort-imports

--- base_report_to_printer/static/src/js/qweb_action_manager.esm.js
+++ base_report_to_printer/static/src/js/qweb_action_manager.esm.js
@@ -1,5 +1,5 @@
-import {markup} from "@odoo/owl";
 import {_t} from "@web/core/l10n/translation";
+import {markup} from "@odoo/owl";
 import {registry} from "@web/core/registry";
 
 async function cupsReportActionHandler(action, options, env) {
