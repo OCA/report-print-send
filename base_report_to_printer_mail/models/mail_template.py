72e5839ab54f172c12adb920155f83105b57ca12
arantxa-s73 <arantxa@studio73.es>
2026-01-13 13:14:10 +0100

    [MIG] base_report_to_printer_mail: Migration to 19.0

--- base_report_to_printer_mail/models/mail_template.py
+++ base_report_to_printer_mail/models/mail_template.py
@@ -4,8 +4,17 @@ from odoo import models
 class MailTemplate(models.Model):
     _inherit = "mail.template"
 
-    def _generate_template(self, res_ids, render_fields, find_or_create_partners=False):
+    def _generate_template(
+        self,
+        res_ids,
+        render_fields,
+        recipients_allow_suggested=False,
+        find_or_create_partners=False,
+    ):
         self = self.with_context(must_skip_send_to_printer=True)
         return super()._generate_template(
-            res_ids, render_fields, find_or_create_partners=find_or_create_partners
+            res_ids,
+            render_fields,
+            recipients_allow_suggested=recipients_allow_suggested,
+            find_or_create_partners=find_or_create_partners,
         )
